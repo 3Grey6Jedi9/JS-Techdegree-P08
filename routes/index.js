352972c3a7cfbd4304917d2c4ce7b0cdc268fd49
Darth Vader <danieltarancon@gmail.com>
2023-09-21 18:01:16 +0200

    21 1801

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  // Delete or comment out the res.render() method.
+
+  // Asynchronously get all the books using the findAll() method and store them in a variable.
+  const books = await Book.findAll();
+
+  // Log out the books variable to the console.
+  console.log(books);
+
+  // Use the res.json() method to display the books on a webpage.
+  res.json(books);
 });
 
 module.exports = router;
+
+
