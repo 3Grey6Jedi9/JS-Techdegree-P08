cdcbd9fcf673461f3ae887f14537d797fea054a5
Darth Vader <danieltarancon@gmail.com>
2023-09-21 15:53:56 +0200

    21 1555

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const pug = require('pug');
-const sequelize = require('./models');
+const sequelize = require('./models').sequelize;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +24,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+
+
+// Connect to the database
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to the database:', err);
+  });
+
+
+
+// Sync the model with the database
+sequelize.sync()
+  .then(() => {
+    console.log('Model synced with the database');
+  })
+  .catch((err) => {
+    console.error('Failed to sync model with the database:', err);
+  });
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
